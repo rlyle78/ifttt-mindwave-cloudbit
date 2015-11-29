897904f1b4638b1e7e875c7a313d415335860b03
Ron Dagdag <rlyle78@gmail.com>
2015-11-29 12:54:21 -0600

    initial

---
+++ ifttt.js
@@ -0,0 +1,16 @@
+//var config = require('nconf');
+var apiKey = 'dZAP6jLtUo51sCdprJLEIv';
+//var apiKey = config.util.getEnv('NODE_ENV')
+//var apiKey = config.get('NODE_ENV');
+console.log(apiKey);  
+var IFTTTMaker = require('iftttmaker')(apiKey);
+ 
+var action = 'MindControlTrigger';
+ 
+IFTTTMaker.send(action, function (error) {
+  if (error) {
+    console.log('The request could not be sent:', error);
+  } else {
+    console.log('Request was sent');
+  }
+});
---
+++ index.js
@@ -0,0 +1,55 @@
+var Cylon = require('cylon');
+var Barcli = require("barcli");
+var attentionGraph = new Barcli({
+  label: "attention Graph",
+  range: [0, 100],
+});
+
+var apiKey = 'dZAP6jLtUo51sCdprJLEIv';
+var IFTTTMaker = require('iftttmaker')(apiKey);
+var trigger_on = 'MindControlTrigger_On';
+var trigger_off = 'MindControlTrigger_Off';
+
+Cylon.robot({
+  connections: {
+    neurosky: { adaptor: 'neurosky', port: '/dev/cu.MindWaveMobile-DevA' }
+  },
+
+  devices: {
+    headset: { driver: 'neurosky' }
+  },
+
+  work: function(my) {
+    my.headset.on('attention', function(data) {
+      //Logger.info("attention:" + data);
+      if (data < 100)
+      {
+        attentionGraph.update(data);
+        if (data > 50)
+        {
+            IFTTTMaker.send(trigger_on, function (error) {
+              if (error) {
+                console.log('The request could not be sent:', error);
+              } else {
+                console.log('Request was sent');
+              }
+            });
+        } else
+        {
+            IFTTTMaker.send(trigger_off, function (error) {
+              if (error) {
+                console.log('The request could not be sent:', error);
+              } else {
+                console.log('Request was sent');
+              }
+            });
+        }
+      }
+    });
+
+    /*my.headset.on('meditation', function(data) {
+      Logger.info("meditation:" + data);
+    });*/
+    
+  }
+}).start();
